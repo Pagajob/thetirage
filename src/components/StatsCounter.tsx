fd2457361d03f88dd43cd454ca388644e4288df6
Pagajob <pagajobteam@gmail.com>
2025-09-25 21:04:54 +0200

    Updated StatsCounter.tsx

--- src/components/StatsCounter.tsx
+++ src/components/StatsCounter.tsx
@@ -129,7 +129,7 @@ const StatsCounter: React.FC = () => {
               alt="iPhone"
               className="w-64 h-auto opacity-80 drop-shadow-2xl"
             />
-            <div className="absolute -bottom-4 -right-4 bg-yellow-400 text-blue-900 px-4 py-2 rounded-xl font-bold shadow-lg">
+            <div className="absolute -bottom-4 -right-4 bg-yellow-400 text-blue-900 px-4 py-2 rounded-full font-bold shadow-lg">
               Jackpot !
             </div>
           </div>
