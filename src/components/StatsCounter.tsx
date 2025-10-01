9c7379396a3d5468110e89b92add49c5dd49f95f
Pagajob <pagajobteam@gmail.com>
2025-10-01 22:19:43 +0200

    Updated StatsCounter.tsx

--- src/components/StatsCounter.tsx
+++ src/components/StatsCounter.tsx
@@ -33,7 +33,7 @@ const StatsCounter: React.FC = () => {
 
         // Base de 147 participants + nouveaux participants (tickets)
         const baseParticipations = 147;
-        const newParticipants = tickets?.length || 5;
+        const newParticipants = tickets?.length || 8;
         const totalParticipations = baseParticipations + newParticipants;
         
         setParticipations(totalParticipations);
